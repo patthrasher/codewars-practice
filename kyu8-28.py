a02dd501dcadfb8577bff553ac893e2598e02b9c
patthrasher <patrickalanthrasher@gmail.com>
2020-10-13 12:07:00 -0500

    added kata

---
+++ kyu8-28.py
@@ -0,0 +1,17 @@
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+def areYouPlayingBanjo(name) :
+    output = ''
+    if name[0].lower() == 'r' :
+        output = ' plays banjo'
+    else :
+        output = ' does not play banjo'
+        
+    return name + output
+
+
+
+print(areYouPlayingBanjo('Pat'))
+print(areYouPlayingBanjo('Rat'))
+print(areYouPlayingBanjo('rat'))
