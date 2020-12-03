6e1103b1a6a52760e31bdb2183e7eefff7971b56
patthrasher <patrickalanthrasher@gmail.com>
2020-12-03 09:21:14 -0600

    added solution to kyu7-33.py

--- kyu7-33.py
+++ kyu7-33.py
@@ -6,8 +6,14 @@ def valid_spacing(s) :
     # return True
 
     # one line solution
-    return False if s.startswith(' ') or s.endswith(' ') or '  ' in s else True
+    # return False if s.startswith(' ') or s.endswith(' ') or '  ' in s else True
 
+    # regular expression solution
+    import re
+    return False if re.search('^ ', s) or re.search(' $', s) or re.search('  ', s) else True
 
-print(valid_spacing('Hello world'))
-print(valid_spacing(' Hello world'))
+print(valid_spacing('Hello world')) # True
+print(valid_spacing('Hello world ')) # False
+print(valid_spacing('Hello  world')) # False
+print(valid_spacing(' Hello world')) # False
+print(valid_spacing('Hello world')) # True
