c4e9e50615f9d1df6a38b5a4bb33597db030c7cd
patthrasher <patrickalanthrasher@gmail.com>
2020-12-13 23:18:43 -0600

    added regex solution

--- kyu6-11.py
+++ kyu6-11.py
@@ -1,15 +1,20 @@
 def solution(s) :
-    if len(s) % 2 == 1 :
-        s = s + '_'
-    l = []
-    ns = ''
-    for each in s :
-        ns = ns + each
-        if len(ns) == 2 :
-            l.append(ns)
-            ns = ''
-    return l
+    # first solution
+    # if len(s) % 2 == 1 :
+    #     s = s + '_'
+    # l = []
+    # ns = ''
+    # for each in s :
+    #     ns = ns + each
+    #     if len(ns) == 2 :
+    #         l.append(ns)
+    #         ns = ''
+    # return l
 
+    # regex solution
+    import re
+    s = s + '_' if len(s) % 2 == 1 else s
+    return re.findall('..', s)
 
 print(solution("asdfadsf")) # ['as', 'df', 'ad', 'sf']
 print(solution("asdfads")) # ['as', 'df', 'ad', 's_']
