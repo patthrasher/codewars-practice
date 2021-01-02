34850a295d9eee48f6354775dcc4505c6684f3ad
patthrasher <patrickalanthrasher@gmail.com>
2021-01-01 20:30:41 -0600

    refactored but still too slow

--- kyu5-5.py
+++ kyu5-5.py
@@ -1,16 +1,28 @@
 from math import sqrt
 
 def list_squared(m, n) :
+    # works but needs to be optimized for speed
+    # output = []
+    # for each in range(m, n + 1) :
+    #     l = []
+    #     for i in range(1, each + 1) :
+    #         if each % i == 0 :
+    #             l.append(i * i)
+    #
+    #     tot = sum(l)
+    #     root = sqrt(tot)
+    #     if str(root).endswith('.0') :
+    #         output.append([each, tot])
+    #
+    # return output
+
+    # list comprehension, still too slow
     output = []
     for each in range(m, n + 1) :
-        l = []
-        for i in range(1, each + 1) :
-            if each % i == 0 :
-                l.append(i * i)
 
-        tot = sum(l)
-        root = sqrt(tot)
-        if str(root).endswith('.0') :
+        tot = sum([i * i for i in range(1, each + 1) if each % i == 0])
+
+        if str(sqrt(tot)).endswith('.0') :
             output.append([each, tot])
 
     return output
