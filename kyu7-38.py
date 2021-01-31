62c789e50deea05fa1c42aa10791cd2e64ca01d8
patthrasher <patrickalanthrasher@gmail.com>
2021-01-30 19:37:07 -0600

    added comments

--- kyu7-38.py
+++ kyu7-38.py
@@ -1,4 +1,5 @@
 def averages(arr) :
+    # first solution
     # if arr == None or len(arr) < 2 :
     #     return []
     # l = []
@@ -11,6 +12,7 @@ def averages(arr) :
     #
     # return l
 
+    # second solution
     return [sum(i) / 2 for i in zip(arr, arr[1:])] if arr != None else []
 
 print(averages([1, 3, 5, 1, -10])) # [2, 4, 3, -4.5]
