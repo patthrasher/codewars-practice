24b65e281ba53d707a06a195c0cee8c906c6897b
patthrasher <patrickalanthrasher@gmail.com>
2021-01-30 19:35:59 -0600

    edited kyu7-38.py, refactored

--- kyu7-38.py
+++ kyu7-38.py
@@ -1,14 +1,17 @@
 def averages(arr) :
-    if arr == None or len(arr) < 2 :
-        return []
-    l = []
-    i = 1
-    while i < len(arr) :
-        x = arr[i - 1] + arr[i]
-        av = x / 2
-        l.append(av)
-        i = i + 1
+    # if arr == None or len(arr) < 2 :
+    #     return []
+    # l = []
+    # i = 1
+    # while i < len(arr) :
+    #     x = arr[i - 1] + arr[i]
+    #     av = x / 2
+    #     l.append(av)
+    #     i = i + 1
+    #
+    # return l
 
-    return l
+    return [sum(i) / 2 for i in zip(arr, arr[1:])] if arr != None else []
 
 print(averages([1, 3, 5, 1, -10])) # [2, 4, 3, -4.5]
+print(averages(None))
